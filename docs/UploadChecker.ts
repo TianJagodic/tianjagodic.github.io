6235d71ea313a7fbbd7cb63bb5c69885554ef89c
Tian <tian.jago@gmail.com>
2019-05-30 00:10:52 +0200

    Does this actualy work?

---
+++ docs/UploadChecker.ts
@@ -0,0 +1,12 @@
+function checkIfLoggedIn(){
+
+    if(document.getElementById("logInText").textContent == "LOGIN"){
+        console.log("The user is not logged in!");
+        document.getElementById("dragAndDrop").hidden = true;
+        document.getElementById("plsLogIn").innerText = "You are not logged in! Please log in!";
+    }
+    else
+        console.log("The user is logged in!")
+}
+
+checkIfLoggedIn();
