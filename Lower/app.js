2251cae5715d37505a79e53183ea1b3e3871c6ff
Tian <tian.jago@gmail.com>
2019-06-04 12:17:31 +0200

    Logo fix

--- Lower/app.js
+++ Lower/app.js
@@ -1,11 +1,19 @@
-const ImageList = document.querySelector('#listOfImages);
+const ImageList = document.querySelector('#listOfImages');
 
     function renderImage(doc){
         let img = document.createElement('img');
         let name = document.createElement('span');
 
         name.textContent = doc.data().name;
+        url= doc.data().url;
         img.appendChild(name);
+        img.className = "imgRenders";
+
+
+        var test = url;
+        img.src= test;
+
+
 
         ImageList.appendChild(img);
     }
